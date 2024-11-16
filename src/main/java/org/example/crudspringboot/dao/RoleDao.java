a5196ed779af6421d2440c0a31fdb459a15420bd
NikitaWelov <nikqwel@gmail.com>
2024-11-16 20:44:21 +0300

    RoleService

--- src/main/java/org/example/crudspringboot/dao/RoleDao.java
+++ src/main/java/org/example/crudspringboot/dao/RoleDao.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RoleDao extends JpaRepository<Role, Integer> {
     Role findById(Long id);
+
+    Role findByName(String name);
 }
---
+++ src/main/java/org/example/crudspringboot/service/RoleService.java
@@ -0,0 +1,29 @@
+package org.example.crudspringboot.service;
+
+import jakarta.transaction.Transactional;
+import org.example.crudspringboot.dao.RoleDao;
+import org.example.crudspringboot.model.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+
+    private final RoleDao roleDao;
+
+    @Autowired
+    public RoleService(RoleDao roleDao) {
+        this.roleDao = roleDao;
+    }
+
+    @Transactional
+    public Role getRoleById(Long id) {
+        return roleDao.findById(id);
+    }
+
+    @Transactional
+    public Role getDefaultRole() {
+        Role defaultRole = roleDao.findByName(Role.RoleType.USER.name());
+        return defaultRole;
+    }
+}
