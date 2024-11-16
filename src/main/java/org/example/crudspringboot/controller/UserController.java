53702f35881a8d65980961628b4314ffad96899a
NikitaWelov <nikqwel@gmail.com>
2024-11-16 17:09:29 +0300

    com123

--- src/main/java/org/example/crudspringboot/controller/UserController.java
+++ src/main/java/org/example/crudspringboot/controller/UserController.java
@@ -1,12 +1,17 @@
 package org.example.crudspringboot.controller;
 
+import org.example.crudspringboot.model.User;
 import org.example.crudspringboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -19,7 +24,16 @@ public class UserController {
     @GetMapping("/users")
     public String getAllUsers(Model model) {
      model.addAttribute("users", userService.getAllUsers());
-     return "users";
+     model.addAttribute("user", new User());
+     return "user-management";
     }
+
+    @PostMapping
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+
 
 }
